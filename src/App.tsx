7e638349d32f320002a5907d016081c48b3ebb39
FrisonJr <jair.frison@gmail.com>
2023-06-30 16:50:33 -0300

    parallex test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import "./App.css";
+import moon from "./images/moon.png";
+import land from "./images/land.png";
+import cat from "./images/cat.gif";
 
 function App() {
+  const ref = React.useRef();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Parallax pages={4} ref={ref as any}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: "cover",
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <img src={cat} alt="cat" />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          // @ts-ignore
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Gato</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          // @ts-ignore
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Cheguei</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
